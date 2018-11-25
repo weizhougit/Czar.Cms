057dab55f282b78d4a07410926753456d228fd65
weizhou <2503998380@q.com>
2018-11-25 13:29:00 +0800

    新增第三篇文章的事例代码

---
+++ demoCore001/demoCore001/Models/ContentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using demoCore001.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace demoCore001
+{
+    public class ContentViewModel
+    {
+        /// <summary>
+        /// 内容列表
+        /// </summary>
+        public List<Content> Contents { get; set; }
+    }
+}
